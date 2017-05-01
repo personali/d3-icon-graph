2ed63505becf72be1cbbbd679dee004315220149
Ron Buenavida <rbuenavida@gmail.com>
2017-05-01 09:18:46 +0300

    bypass + fixed issue in chrome backgroud dissappearing bypass + started work on layout of insite page

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ global.d3.iconGraph = function(selector){
            if(!properties.percent)
                properties.percent = 100;
 
-           element.style('background', 'url(' + properties.emptyGraph+ ')')
+           element.style('background', 'url(' + properties.emptyGraph + ')')
                .style('width', properties.width)
                .style('position', 'relative')
                .style('height', properties.height);
 
-           var d3graph = element.append('div');
 
-           d3graph.style('width', properties.width);
-           d3graph.style('height',properties.height);
-           d3graph.style('position', 'absolute');
-           d3graph.style('clip','rect('+properties.height+ ' ' + properties.width+ ' ' + properties.height + ' 0px)');
+           var d3graph = element.append('div');
            d3graph.style('background','url('+ properties.fullGraph +')');
+           d3graph.style('width', properties.width);
+           d3graph.style('height', properties.height);
+           d3graph.style('position', 'absolute');
+           d3graph.style('z-index', '1000');
+           d3graph.style('clip','rect(' + properties.height + ' ' + properties.width + ' ' + properties.height + ' 0px)');
 
            var height = parseFloat(properties.height.replace( /^\D+/g, ''))*(1 - (properties.percent/100));
 
-           var t = d3.transition().duration(3500);
+           var t = d3.transition().duration(2000);
            d3graph.transition(t).style('clip','rect(' + height + 'px ' + properties.width + ' ' + properties.height + ' 0px)');
-
        }
    };
 
