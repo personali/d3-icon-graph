832282719e16f6e9104e1c4c1c2325aaeb433e6c
Ron Buenavida <rbuenavida@gmail.com>
2017-05-10 11:05:46 +0300

    bypass + more charts

--- index.js
+++ index.js
@@ -82,3 +82,135 @@ global.d3.barGraph = function(selector) {
   return barGraph;
 }
 
+
+global.d3.scoreCard = function(selector) {
+    var element = d3.select(selector)
+
+    var scoreCard = {
+      properties: function(props) {
+        var colors = {
+            'pink': '#E1499A',
+            'yellow': '#f0ff08',
+            'green': '#47e495'
+        };
+
+        var color = '#ffffff';
+
+        var radius = 75;
+        var border = 5;
+        var padding = 3;
+        var startPercent = 0;
+        var endPercent = 0.53;
+
+        var twoPi = Math.PI * 2;
+        var formatPercent = d3.format('.0%');
+        var formatNumber = d3.format('0');
+        var boxSize = (radius + padding) * 2;
+
+        var count = Math.abs((endPercent - startPercent) / 0.01);
+        var step = endPercent < startPercent ? -0.01 : 0.01;
+
+        var parent = element;
+
+        var svg = parent.append('svg')
+            .attr('width', boxSize)
+            .attr('height', boxSize);
+
+        var arc = d3.arc()
+            .startAngle(0)
+            .innerRadius(radius)
+            .outerRadius(radius - border);
+
+        var defs = svg.append('defs');
+
+        var filter = defs.append('filter')
+            .attr('id', 'blur');
+
+        /*
+        filter.append('feGaussianBlur')
+            .attr('in', 'SourceGraphic')
+            .attr('stdDeviation', '7');
+        */
+
+        var g = svg.append('g')
+            .attr('transform', 'translate(' + boxSize / 2 + ',' + boxSize / 2 + ')');
+
+        var meter = g.append('g')
+            .attr('class', 'progress-meter');
+
+        meter.append('path')
+            .attr('class', 'background')
+            .attr('fill', '#ccc')
+            .attr('fill-opacity', 0.5)
+            .attr('d', arc.endAngle(twoPi));
+
+        var foreground = meter.append('path')
+            .attr('class', 'foreground')
+            .attr('fill', color)
+            .attr('fill-opacity', 1)
+            .attr('stroke', color)
+            .attr('stroke-width', 5)
+            .attr('stroke-opacity', 1)
+            .attr('filter', 'url(#blur)');
+
+        var front = meter.append('path')
+            .attr('class', 'foreground')
+            .attr('fill', color)
+            .attr('fill-opacity', 1);
+
+        var numberText = meter.append('text')
+            .attr('fill', '#fff')
+            .attr('text-anchor', 'middle')
+            .style("font-size", "3em")
+            .attr('dy', '-.10em');
+
+        var justText = meter.append('text')
+            .attr('fill', '#fff')
+            .attr('text-anchor', 'middle')
+            .attr('dy', '1.0em')
+            .text('Out of 100')
+            .style("font-size", "1.5em");
+
+        function updateProgress(progress) {
+            foreground.attr('d', arc.endAngle(twoPi * progress));
+            front.attr('d', arc.endAngle(twoPi * progress));
+            numberText.text(formatNumber(progress*100));
+        }
+
+        var progress = startPercent;
+
+        (function loops() {
+            updateProgress(progress);
+            if (count > 0) {
+                count--;
+                progress += step;
+                setTimeout(loops, 10);
+            }
+        })();
+      }
+    }
+
+    return scoreCard;
+}
+
+global.d3.countUp = function(selector) {
+  var element = d3.select(selector);
+
+  var countUp = {
+    properties: function(props) {
+      var format = d3.format(props.format);
+      element
+        .transition()
+        .duration(props.duration)
+        .tween("text", function() {
+          var that = d3.select(this),
+            i = d3.interpolateNumber(props.start, props.end);
+            return function(t) {
+              that.text(format(i(t)));
+            };
+      });
+    }
+  }
+
+  return countUp;
+}
